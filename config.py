66dca6e647a7c2e9609f048a4d840ed27d4853f3
s-r0ff3y <spencerjack13@gmail.com>
2021-04-06 11:59:38 +0100

    files

---
+++ config.py
@@ -0,0 +1,91 @@
+from youtube_dl import YoutubeDL
+from pytube import Playlist
+from pytube import YouTube
+import urllib.request
+import platform
+import pytube
+import re
+import os
+
+original_path = os.getcwd()
+
+first = 'https://www.youtube.com/results?search_query='
+second = 'https://www.youtube.com/watch?v='
+
+def clear():
+    if 'windows' in platform.system():
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def search(key):
+    count = 0
+    top = []
+    url = first + key # full url
+    html = urllib.request.urlopen(url)
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    for i in video_ids:
+        if count == 3:
+            continue
+        else:
+            top.append(second + i)
+            count += 1
+    return top
+
+def download(mode, url, path):
+    if mode == 3 or mode == 4:
+        pass
+    else:
+        print('\n[+] Downloading Top Result...')
+    if mode == 1:
+        opts = {
+            'format':'bestaudio/best',
+            'postprocessors':[{
+                'key':'FFmpegExtractAudio',
+                'preferredcodec':'mp3',
+                'preferredquality':'192'
+            }]
+        }
+        ydl = YoutubeDL(opts)
+        os.chdir(path)
+        ydl.download([url])
+        os.chdir(original_path)
+        return True
+    elif mode == 2:
+        opts = {}
+        ydl = YoutubeDL(opts)
+        os.chdir(path)
+        ydl.download([url])
+        os.chdir(original_path)
+        return True
+    elif mode == 3: # mp3 playlist
+        opts = {
+            'format':'bestaudio/best',
+            'postprocessors':[{
+                'key':'FFmpegExtractAudio',
+                'preferredcodec':'mp3',
+                'preferredquality':'192'
+            }]
+        }
+        ydl = YoutubeDL(opts)
+        os.chdir(path)
+        playlist = Playlist(url)
+        print('[-] Downloading {} Videos...'.format(len(playlist.video_urls)))
+        for url in playlist:
+            print('[-] Downloading {}...'.format(url))
+            ydl.download([url])
+        os.chdir(original_path)
+        return True
+    elif mode == 4:
+        opts = {}
+        ydl = YoutubeDL(opts)
+        playlist = Playlist(url)
+        os.chdir(path)
+        print('[-] Downloading {} Videos...'.format(len(playlist.video_urls)))
+        for url in playlist:
+            print('[-] Downloading {}...'.format(url))
+            ydl.download([url])
+        os.chdir(original_path)
+        return True
+    else:
+        return False
---
+++ main.py
@@ -0,0 +1,104 @@
+from config import *
+
+msg = ''' __   __                  __        __   ___  __  
+|  \ /  \ |  | |\ | |    /  \  /\  |  \ |__  |__) 
+|__/ \__/ |/\| | \| |___ \__/ /~~\ |__/ |___ |  \ 
+                                            By s-r0ff3y
+'''
+
+def main():
+    mode = 0
+    count = 1
+    
+    clear()
+    print(msg)
+
+    print('-'*8 + '   OPTIONS   ' + '-'*8)
+    print('[1] Download MP3\n[2] Download MP4')
+    option = input('>>> ')
+    print('\n[?] Path to save files?')
+    path = input('>>> ')
+
+    if option == '1':
+        mode = 1
+    elif option == '2':
+        mode = 2
+    else:
+        print('[!] Invalid Option.')
+        exit()
+
+    print('[?] Would you like to download a playlist? (y/n)')
+    playlist = input('>>> ')
+    if playlist.lower().strip() == 'y':
+        if mode == 1: # if mp3
+            mode = 3 # playlist mp3
+            clear()
+            print('-'*8 + '   PLAYLIST URL   ' + '-'*8)
+            url = input('>>> ')
+            if download(mode, url, path):
+                print('[+] Playlist downloaded.')
+                exit()
+            else:
+                print('[!] Error downloading playlist.')
+                exit()
+            exit()
+        elif mode == 2:
+            mode = 4
+            clear()
+            print('-'*8 + '   PLAYLIST URL   ' + '-'*8)
+            url = input('>>> ')
+            if download(mode, url, path):
+                print('[+] Playlist downloaded.')
+                exit()
+            else:
+                print('[!] Error downloading playlist.')
+                exit()
+            exit()
+        else:
+            print('[!] Invalid Option.')
+            exit()
+    else:
+        pass # no playlist
+
+    clear()
+    print('-'*8 + '   ENTER KEY WORDS   ' + '-'*8)
+    key = input('>>> ')
+
+    key = key.replace(' ', '+')
+
+    top = search(key)
+
+    if top:
+        clear()
+        print('-'*8 + '   TOP THREE VIDEOS   ' + '-'*8)
+        for link in top:
+            print('[{}] {}'.format(str(count), link))
+            count += 1
+        if download(mode, top[0], path):
+            print('[+] Download Complete.')
+            input()
+            main()
+        else:
+            print('[!] Error Downloading Video.')
+            input()
+            main()
+    else:
+        print('[!] Error Finding Video.')
+        input()
+        main()
+
+if os.path.isdir('downloads'):
+    pass
+else:
+    os.mkdir('downloads')
+
+main()
+
+'''
+mode 1 = mp3
+mode 2 = mp4
+mode 3 = playlist mp3
+mode 4 = playlist mp4
+'''
+
+# PLAYLIST FUNCTIONS NOT WORKING!
